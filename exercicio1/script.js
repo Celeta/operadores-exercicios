d493a2791a956a11ea2b3f7862edb8bafc5399eb
Celeta <marceloceleta@gmail.com>
2022-11-22 13:01:50 -0300

    alteraçoes no exercicio

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+let numeroPar
+numeroPar = prompt ("Digite um número par...")
+console.log (numeroPar)
+
+numeroPar = numeroPar % 2
+console.log (numeroPar)
+
+
+
+//Ao inserir numeros pares sempre não há resto, impares resta 1.
---
+++ exercicio2/script.js
@@ -0,0 +1,13 @@
+let p1
+let p2
+
+p1 = prompt ("Digite um número qualquer...")
+p2 = prompt ("Digite qualquer outro número...")
+
+console.log(p1)
+console.log(p2)
+
+console.log("1º número é maior que o 2º?:", p1<p2)
+
+console.log("1º número é igual ao 2º?:", p1===p2)
+
